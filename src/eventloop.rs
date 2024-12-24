8a81f9ba25315bca03e949a6e9ca7093083cb789
Nick Allison <nickrallison1@gmail.com>
2024-12-24 14:31:52 -0700

    bit of refactoring

--- src/eventloop.rs
+++ src/eventloop.rs
@@ -1,16 +1,38 @@
 use crate::frame::{Frame, Pixel};
+use rand::{Rng, SeedableRng};
 
 pub trait LoopState {
     fn update(self) -> Self;
-    fn draw<const W:  usize, const H: usize>(&self, frame: Frame<W, H>) -> Frame<W, H>;
+    fn draw<const W: usize, const H: usize>(&self, frame: Frame<W, H>) -> Frame<W, H>;
 }
 
-pub struct ExampleLoopState {}
-impl LoopState for ExampleLoopState {
+pub struct ExampleLoopState<const W: usize, const H: usize> {
+    chem_a: Vec<f32>,
+    chem_b: Vec<f32>,
+}
+
+impl<const W: usize, const H: usize> ExampleLoopState<W, H> {
+    pub fn new() -> Self {
+        let chem_a: Vec<f32> = vec![0.0; W * H];
+        let chem_b: Vec<f32> = vec![0.0; W * H];
+        Self { chem_a, chem_b }
+    }
+
+    pub fn new_seeded(seed: u64) -> Self {
+        let mut rng = rand::rngs::StdRng::seed_from_u64(seed);
+        let chem_a: Vec<f32> = (0..W * H).map(|_| rng.gen_range(0.0..1.0)).collect();
+        let chem_b: Vec<f32> = (0..W * H).map(|_| rng.gen_range(0.0..1.0)).collect();
+        Self { chem_a, chem_b }
+    }
+}
+
+// Add the generic parameters to the `ExampleLoopState` struct in the trait implementation
+impl<const WIDTH: usize, const HEIGHT: usize> LoopState for ExampleLoopState<WIDTH, HEIGHT> {
     fn update(self) -> Self {
         self
     }
-    fn draw<const W:  usize, const H: usize>(&self, mut frame: Frame<W, H>) -> Frame<W, H> {
+
+    fn draw<const W: usize, const H: usize>(&self, mut frame: Frame<W, H>) -> Frame<W, H> {
         for y in 0..H {
             for x in 0..W {
                 let a = (x % 256) as u8;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 #![feature(generic_const_exprs)]
 
-mod frame;
 mod eventloop;
+mod frame;
 
-use minifb::{Key, Window, WindowOptions};
 use crate::eventloop::{ExampleLoopState, LoopState};
 use crate::frame::{Frame, Pixel};
+use minifb::{Key, Window, WindowOptions};
 
 fn main() {
     const WIDTH: usize = 800;
     const HEIGHT: usize = 600;
 
     let mut frame = Frame::<WIDTH, HEIGHT>::new();
-    let loopstate = ExampleLoopState {};
+    let mut loopstate = ExampleLoopState::<WIDTH, HEIGHT>::new();
     frame = loopstate.draw::<WIDTH, HEIGHT>(frame);
 
     let mut window = Window::new(
@@ -30,6 +30,8 @@ fn main() {
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         // Convert the frame buffer to a format minifb understands
+        loopstate = loopstate.update();
+        frame = loopstate.draw::<WIDTH, HEIGHT>(frame);
         let buffer: Vec<u32> = frame.get_pixels();
 
         // Update the window with the buffer
